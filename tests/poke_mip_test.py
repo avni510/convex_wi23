6a912542371b430f8e06869036fd85431a12b9b4
Avni Kothari <avni@8thlight.com>
2023-03-14 11:07:52 -0700

    updated tests

--- tests/poke_mip_test.py
+++ tests/poke_mip_test.py
@@ -10,10 +10,9 @@ class TestMIP(unittest.TestCase):
         m = poke_mip.build(b)
         m.print_information()
         m.solve(log_output=True)
-        # m.objective_value
-        # m.solution.get_value('x1')
         print(m.print_solution())
 
-
-
-
+        self.assertEqual(m.objective_value, 312)
+        self.assertEqual(m.solution.get_value('x1'), 156)
+        self.assertEqual(m.solution.get_value('x2'), 63)
+        self.assertEqual(m.solution.get_value('x3'), 93)
