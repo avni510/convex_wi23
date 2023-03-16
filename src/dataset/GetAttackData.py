1f58c5fc427b569c89ea59e8c73d557019a8624e
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2023-03-16 15:28:50 -0700

    First draft of data generation
    Pokemon can be customized on lines 155 and 156 of GetAttackData.py, or I can add command line arguments for this if that helps!

--- src/dataset/GetAttackData.py
+++ src/dataset/GetAttackData.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import json
 from urllib.request import urlopen
+import requests
+import pandas as pd
 
 ##############################################################################################
 # Starting Code for generating data on attacker damage potential, and defender base stats, 
@@ -46,12 +48,12 @@ def calcDamage(stat, base_power=100):
     return base_power*stat*2/5*1.5
 
 #takes base_stats as an array of 3 ints representing base stats for HP, Def, SpD, and calculates Beta Values
-def calc_betas(base_stats = [80, 80, 80], base_atk=100, base_pow=100, base_special_attack=100, base_special_pow = 100, spread="Serious:0/0/0/0/0/0"): 
+def calc_betas(base_stats = [80, 80, 80], base_offense = [100, 100], base_pows=[100, 100], spread="Serious:0/252/0/252/0/0"): 
     b2 = base_stats[0] + 75 + 32
     b3 = base_stats[1] + 20 + 32
     b4 = base_stats[2] + 20 + 32
-    b1 = calcDamage(calcStat(base_atk, spread, ATK), base_pow)
-    b5 = calcDamage(calcStat(base_special_attack, spread, ATK), base_special_pow)
+    b1 = calcDamage(calcStat(base_offense[0], spread, ATK), base_pows[0])
+    b5 = calcDamage(calcStat(base_offense[1], spread, SPA), base_pows[1])
 
     return [b1, b2, b3, b4, b5]
 
@@ -83,7 +85,89 @@ ATTACK_POWS = [120, 150] #not sure best way to grab this - may default to 100s f
 DEFENDER = 'raichu'
 DEFENDER_STATS = [60, 55, 80]
 
-print(calc_betas(DEFENDER_STATS, ATTACK_STATS[0], ATTACK_POWS[0], ATTACK_STATS[1], ATTACK_POWS[1], spread='Serious:0/252/0/252/0/0'))
+# print(calc_betas(DEFENDER_STATS, ATTACK_STATS[0], ATTACK_POWS[0], ATTACK_STATS[1], ATTACK_POWS[1], spread='Serious:0/252/0/252/0/0'))
+
+
+# takes a pokemon species name as a lower case string
+# returns array of [attack base stat, special attack base stat]
+def get_attack_stats(pokemon): 
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/'+pokemon)
+    if not response.ok:
+        print("error with json request for Pokemon " + pokemon)
+        return -1
+    else: 
+        data = json.loads(response.content)
+        return [data['stats'][ATK]['base_stat'], data['stats'][SPA]['base_stat']]
+
+# returns array of defense stats
+def get_defense_stats(pokemon): 
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/'+pokemon)
+    if not response.ok:
+        print("error with json request for Pokemon " + pokemon)
+        return -1
+    else: 
+        data = json.loads(response.content)
+        return [data['stats'][HP]['base_stat'], data['stats'][DEF]['base_stat'], data['stats'][SPD]['base_stat']]
+
+# print(get_attack_stats('charizard'))
+
+# takes pokemon species name as lower case string
+# returns highest base power physical and special moves of the Pokemon's type
+def get_attack_pows(pokemon): 
+    # return [150, 150] #stub
+    max_pows = [0, 0]
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/'+pokemon)
+    if not response.ok:
+        print("error with json request for Pokemon " + pokemon)
+        return -1
+    else: 
+        data = json.loads(response.content)
+
+        pkmn_types = [] #track types of the pokemon
+        json_for_pkmn_types = data['types']
+        for pkmn_type in json_for_pkmn_types: 
+            pkmn_types.append(pkmn_type['type']['name'])
+
+        moves = data['moves']
+        for move in moves:
+            response = requests.get(move['move']['url'])
+            if not response.ok:
+                print("error with json request for move " + str(move))
+                return -1
+            else: 
+                move_data = json.loads(response.content)
+                if move_data['type']['name'] in pkmn_types:
+                    if move_data['damage_class']['name'] == 'physical': 
+                        if move_data['power'] and (max_pows[0] < move_data['power']): 
+                            max_pows[0] = move_data['power']
+                    elif move_data['damage_class']['name'] == 'special': 
+                        if move_data['power'] and (max_pows[1] < move_data['power']): 
+                            max_pows[1] = move_data['power']
+        
+        return max_pows
+
+# print(get_attack_pows('charizard'))
+
+#################################
+### generate first dataset
+#################################
+
+attackers = ['charizard', 'snorlax']
+defenders = ['snorlax', 'raichu']
+
+df = pd.DataFrame(columns=['b1', 'b2', 'b3', 'b4', 'b5', 'attacker', 'defender'])
+
+for attacker in attackers: 
+    for defender in defenders: 
+        betas = calc_betas(get_defense_stats(defender), get_attack_stats(attacker), get_attack_pows(attacker)) #uses default argument for spread for now
+        df.loc[df.shape[0]] = betas + [attacker, defender]
+
+df.to_csv('dataset-draft-1.csv', index=False)
+        
+
+    
+# #takes move json
+# def check_move(url, type, category='physical')
 
 # Can get usage rates for spreads on attacker, pokemon doing the attacking, maybe base power of attack moves (will need to integrate with API)
 
