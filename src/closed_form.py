5d1b6fa6e38a11cafcbceb26d0fe2fc44b03183f
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2023-03-22 21:43:37 -0700

    fix closed form

--- src/closed_form.py
+++ src/closed_form.py
@@ -21,10 +21,10 @@ def build(b):
         if np.sqrt(b4 + b5) <= b4/(b2 - 32):
             x1 = (np.sqrt(b4 + b5)) #in this case, no better objective function value than this, for this region, and this function lower bounds the other 2, so global optima
         else:#in this case, any x1 value less than b5/(b3 - 32) is worse than picking this value (because the derivative is negative in the region for x1 smaller than this)
-            if np.sqrt(b4) < b4/(b2-32): 
-                x1 = (b4/(b2-32)) # in this case, no better value than b5/(b3-32) because deriv is positive in whole region. This x1 val also lower bounds the region to the right, so global opt
-            elif np.sqrt(b4) < b5/(b3-32):
-                x1 = (np.sqrt(b4)) # in this case, no better value than b4 (deriv of convex function is 0). The value at b5/(b3-32) is worse, and anything smaller is worse (see above)
+            if np.sqrt(b5) < b4/(b2-32): 
+                x1 = (b4/(b2-32)) # in this case, no better value than b4/(b3-32) because deriv is positive in whole region. This x1 val also lower bounds the region to the right, so global opt
+            elif np.sqrt(b5) < b5/(b3-32):
+                x1 = (np.sqrt(b5)) # in this case, no better value than b5 (deriv of convex function is 0). The value at b4/(b3-32) is worse, and anything smaller is worse (see above)
             else: #in this case, any x1 val less than b4/(b2-32) is worse, because derivative is negative throughout both other regions
                 x1 = (b5/(b3-32)) #deriv here is positive always, so take smallest value in region
 
